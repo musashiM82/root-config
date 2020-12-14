a0e9321a64b038da9cf1f3960211d69ba09c9703
Joel Denning <joeldenning@gmail.com>
2020-12-14 11:38:59 -0700

    Fix race condition

--- src/example-root-config.js
+++ src/example-root-config.js
@@ -1,16 +1,16 @@
 import { registerApplication, start } from "single-spa";
 import {
-    constructApplications,
-    constructRoutes,
-    constructLayoutEngine,
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
 } from "single-spa-layout";
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
 const applications = constructApplications({
-    routes,
-    loadApp({ name }) {
-        return System.import(name);
-    },
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
 });
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
